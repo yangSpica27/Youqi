e5cb960dd830e0b55aa63413d1ac153afb084ab7
SPICa 27 <yangweizhi@tillusory.cn>
2021-11-12 17:15:10 +0800

    # add main page

--- app/src/main/java/com/spica/app/ui/main/MainActivity.kt
+++ app/src/main/java/com/spica/app/ui/main/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.spica.app.ui.main
 
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
+import com.gyf.immersionbar.ktx.immersionBar
+import com.gyf.immersionbar.ktx.statusBarHeight
 import com.spica.app.base.BindingActivity
 import com.spica.app.databinding.ActivityMainBinding
+import com.spica.app.extensions.dp
 import dagger.hilt.android.AndroidEntryPoint
-
 
 /**
  * 主页面
@@ -13,8 +17,30 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>() {
 
 
+    private var colors =
+        intArrayOf(
+            Color.parseColor("#64B5F6"),
+            Color.parseColor("#4FC3F7")
+        )
+    private var bg = GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors)
+
     override fun initializer() {
 
+        immersionBar() {
+            transparentStatusBar()
+            statusBarDarkFont(true)
+            transparentNavigationBar()
+        }
+
+
+        viewBinding.root.setPadding(
+            viewBinding.root.paddingLeft,
+            statusBarHeight + 24.dp,
+            viewBinding.root.paddingRight,
+            viewBinding.root.paddingBottom
+        )
+
+        viewBinding.root.background = bg
     }
 
     override fun setupViewBinding(inflater: LayoutInflater):
