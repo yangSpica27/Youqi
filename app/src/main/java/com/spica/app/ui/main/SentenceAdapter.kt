6cc13ab7fc72b5a2cd1a483be849c3556cf07a36
Heze14 <yangweizhi@toivan.com>
2022-01-14 13:49:55 +0800

    # image item add round corners

--- app/src/main/java/com/spica/app/ui/main/SentenceAdapter.kt
+++ app/src/main/java/com/spica/app/ui/main/SentenceAdapter.kt
@@ -4,26 +4,35 @@ import android.app.Activity
 import android.app.ActivityOptions
 import android.content.Intent
 import coil.load
+import coil.transform.RoundedCornersTransformation
 import com.chad.library.adapter.base.BaseMultiItemQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.spica.app.R
 import com.spica.app.databinding.ItemDateArticleBinding
 import com.spica.app.databinding.ItemDateNormalBinding
 import com.spica.app.databinding.ItemDatePicBinding
+import com.spica.app.extensions.dp
 import com.spica.app.extensions.hide
 import com.spica.app.extensions.show
 import com.spica.app.model.date.*
 import com.spica.app.tools.doOnMainThreadIdle
 import com.spica.app.ui.detail.DetailActivity
 
+/**
+ * 每日句子的适配器
+ */
 class SentenceAdapter(val activity: Activity) :
     BaseMultiItemQuickAdapter<YData, BaseViewHolder>() {
 
 
     init {
+        // 添加普通类型
         addItemType(NORMAL,R.layout.item_date_normal)
+        // 音频类型
         addItemType(AUDIO,R.layout.item_date_normal)
+        // 文章类型
         addItemType(ARTICLE,R.layout.item_date_article)
+        // 图片类型
         addItemType(PIC,R.layout.item_date_pic)
     }
 
@@ -82,6 +91,7 @@ class SentenceAdapter(val activity: Activity) :
                 itemBinding.tvFrom.text = item.content.title
                 doOnMainThreadIdle({
                     itemBinding.ivPic.load(item.content.picture){
+                        transformations(RoundedCornersTransformation(6.dp.toFloat()))
                     }
                 })
 
